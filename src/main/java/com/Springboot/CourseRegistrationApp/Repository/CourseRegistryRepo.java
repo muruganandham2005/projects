f93e3410ff0c966b3b3f130a90b9949a703c4e60
Muruganandham.M <muruganandham5262@gmail.com>
2025-03-03 19:41:37 +0530

    Delete src directory

--- src/main/java/com/Springboot/CourseRegistrationApp/Repository/CourseRegistryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Springboot.CourseRegistrationApp.Repository;
-
-import com.Springboot.CourseRegistrationApp.Model.CourseRegistry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRegistryRepo extends JpaRepository<CourseRegistry,Integer> {
-}
--- src/main/java/com/Springboot/CourseRegistrationApp/Repository/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Springboot.CourseRegistrationApp.Repository;
-
-import com.Springboot.CourseRegistrationApp.Model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepo extends JpaRepository<Course,Integer> {
-}
